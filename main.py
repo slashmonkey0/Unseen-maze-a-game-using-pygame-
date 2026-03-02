9584345d3fce7502ba467985975e9cc3f4953ded
slashmonkey0 <slashmonkey0@gmail.com>
2026-03-02 16:40:55 +0500

    Refactor maze_seeker and add movement controls
    Refactor maze_seeker class to include movement logic and update drawing methods. Implement keyboard controls for player movement.

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ wn=pygame.display.set_mode((640,640))
 class maze_seeker:
     x=0
     y=0
-    def __init__(self,x,y):
+    def draw(self,surface,x,y):
         self.x=x
         self.y=y
-    def draw(self,surface):
         pygame.draw.circle(surface,(255,0,0),(self.x,self.y),21)
-    
-    def move(self,dx,dy):
-        if dx>576 or dx<64:
-            return 
-        if dy>576 or dy<64:
-            return
+
+    def draw_move(self,surface,dx,dy):
         self.x+=dx
         self.y+=dy
-    
+        if self.x<64 or self.x>576 or self.y<64 or self.y>576:
+            return [self.x-dx,self.y-dy]
+        self.draw(surface,self.x,self.y)
+        return [self.x,self.y]
+
 def draw_grid(surface):
     start_x=64
     start_y=64
@@ -38,9 +37,9 @@ def draw_grid(surface):
 def draw_destination(surface,x,y):
     pygame.draw.circle(surface,(0,255,0),(x,y),21)
 
-seeker= maze_seeker(96,544)
-seeker.draw(wn)
-
+seeker= maze_seeker()
+seeker.draw(wn,96,544)
+newPosition=[96,544]
 running=True
 draw_grid(wn)
 draw_destination(wn,544,96)
@@ -49,6 +48,24 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                newPosition=seeker.draw_move(wn,0,-64)
+                print("W pressed once")
+            if event.key == pygame.K_a:
+                newPosition=seeker.draw_move(wn,-64,0)
+                print("A pressed once")
+            if event.key == pygame.K_s:
+                newPosition=seeker.draw_move(wn,0,64)
+                print("S pressed once")
+            if event.key == pygame.K_d:
+                newPosition=seeker.draw_move(wn,64,0)
+                print("D pressed once")
+    wn.fill((0, 0, 0))        # Clear screen
+    draw_grid(wn) # Redraw grid
+    draw_destination(wn,544,96)            
+    seeker.draw(wn,newPosition[0],newPosition[1])           # Redraw player
+    pygame.display.update()   # Update screen
 
 
 
